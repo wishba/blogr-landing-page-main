5d4a770ffbd972b43e995d43a511fed4365dc4ae
wishba <wisnubayu34@gmail.com>
2024-04-25 12:21:10 +0700

    nav style in small screen

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function openNav() {
   const navInside = document.getElementById("navInside");
   const menuBtn = document.getElementById("menuBtn");
   const closeBtn = document.getElementById("closeBtn");
-  navInside.className = "nav__inside nav__inside-opened";
+  navInside.className = "nav__inside nav__inside--opened";
   menuBtn.className = "nav__menu nav__menu-invisible";
   closeBtn.className = "nav__close nav__close-visible";
 }
@@ -19,8 +19,8 @@ function closeNav() {
 function openProduct() {
   const navProduct = document.getElementById("navProduct");
   if (navProduct.className == "nav__product-inside") {
-    navProduct.className = "nav__product-inside nav__product-inside-opened";
-  } else if (navProduct.className == "nav__product-inside nav__product-inside-opened") {
+    navProduct.className = "nav__product-inside nav__product-inside--opened";
+  } else if (navProduct.className == "nav__product-inside nav__product-inside--opened") {
     navProduct.className = "nav__product-inside";
   }
 }
@@ -28,8 +28,8 @@ function openProduct() {
 function openCompany() {
   const navCompany = document.getElementById("navCompany");
   if (navCompany.className == "nav__company-inside") {
-    navCompany.className = "nav__company-inside nav__company-inside-opened";
-  } else if (navCompany.className == "nav__company-inside nav__company-inside-opened") {
+    navCompany.className = "nav__company-inside nav__company-inside--opened";
+  } else if (navCompany.className == "nav__company-inside nav__company-inside--opened") {
     navCompany.className = "nav__company-inside";
   }
 }
@@ -37,8 +37,8 @@ function openCompany() {
 function openConnect() {
   const navConnect = document.getElementById("navConnect");
   if (navConnect.className == "nav__connect-inside") {
-    navConnect.className = "nav__connect-inside nav__connect-inside-opened";
-  } else if (navConnect.className == "nav__connect-inside nav__connect-inside-opened") {
+    navConnect.className = "nav__connect-inside nav__connect-inside--opened";
+  } else if (navConnect.className == "nav__connect-inside nav__connect-inside--opened") {
     navConnect.className = "nav__connect-inside";
   }
 }
